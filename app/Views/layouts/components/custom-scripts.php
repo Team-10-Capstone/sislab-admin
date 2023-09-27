be16bd6c665c6deac56facaf56168900e467fb79
mudzikalfahri <alfahrimudzik@gmail.com>
2023-09-27 21:08:50 +0700

    transition alert

--- app/Views/layouts/components/custom-scripts.php
+++ app/Views/layouts/components/custom-scripts.php
@@ -1,11 +1,20 @@
-    
-        <!-- popperjs -->
-        <script src="<?php echo base_url('assets/libs/@popperjs/core/umd/popper.min.js'); ?>"></script>
+<!-- popperjs -->
+<script src="<?php echo base_url('assets/libs/@popperjs/core/umd/popper.min.js'); ?>"></script>
 
-        <!-- Custom-Switcher JS -->
-        <script src="<?php echo base_url('assets/js/custom-switcher.js'); ?>"></script>
+<!-- Custom-Switcher JS -->
+<script src="<?php echo base_url('assets/js/custom-switcher.js'); ?>"></script>
 
-        <!-- Preline JS -->
-        <script src="<?php echo base_url('assets/libs/preline/preline.js'); ?>"></script>
+<!-- Preline JS -->
+<script src="<?php echo base_url('assets/libs/preline/preline.js'); ?>"></script>
 
-        <?= $this->renderSection('scripts'); ?>
+<?= $this->renderSection('scripts'); ?>
+
+<!-- auto close alert -->
+<script>
+        setTimeout(function () {
+                var toastContainer = document.querySelector('.toast-container');
+                if (toastContainer) {
+                        toastContainer.style.transform = 'translateY(-200%)';
+                }
+        }, 3000);
+</script>
--- app/Views/layouts/components/scripts.php
+++ app/Views/layouts/components/scripts.php
@@ -1,36 +1,46 @@
-		
-        <!-- Back To Top -->
-        <div class="scrollToTop">
-            <span class="arrow"><i class="ri-arrow-up-s-fill text-xl"></i></span>
-        </div>
+<!-- Back To Top -->
+<div class="scrollToTop">
+    <span class="arrow"><i class="ri-arrow-up-s-fill text-xl"></i></span>
+</div>
 
-        <div id="responsive-overlay"></div>
+<div id="responsive-overlay"></div>
 
-		<!-- popperjs -->
-		<script src="<?php echo base_url('assets/libs/@popperjs/core/umd/popper.min.js'); ?>"></script>
+<!-- popperjs -->
+<script src="<?php echo base_url('assets/libs/@popperjs/core/umd/popper.min.js'); ?>"></script>
 
-		<!-- Color Picker JS -->
-		<script src="<?php echo base_url('assets/libs/@simonwep/pickr/pickr.es5.min.js'); ?>"></script>
+<!-- Color Picker JS -->
+<script src="<?php echo base_url('assets/libs/@simonwep/pickr/pickr.es5.min.js'); ?>"></script>
 
-        <!-- sidebar JS -->
-        <script src="<?php echo base_url('assets/js/defaultmenu.js'); ?>"></script>
+<!-- sidebar JS -->
+<script src="<?php echo base_url('assets/js/defaultmenu.js'); ?>"></script>
 
-        <!-- sticky JS -->
-        <script src="<?php echo base_url('assets/js/sticky.js'); ?>"></script>
+<!-- sticky JS -->
+<script src="<?php echo base_url('assets/js/sticky.js'); ?>"></script>
 
-        <!-- Switch JS -->
-        <script src="<?php echo base_url('assets/js/switch.js'); ?>"></script>
+<!-- Switch JS -->
+<script src="<?php echo base_url('assets/js/switch.js'); ?>"></script>
 
-        <!-- Preline JS -->
-        <script src="<?php echo base_url('assets/libs/preline/preline.js'); ?>"></script>
+<!-- Preline JS -->
+<script src="<?php echo base_url('assets/libs/preline/preline.js'); ?>"></script>
 
-        <!-- Simplebar JS -->
-        <script src="<?php echo base_url('assets/libs/simplebar/simplebar.min.js'); ?>"></script>
+<!-- Simplebar JS -->
+<script src="<?php echo base_url('assets/libs/simplebar/simplebar.min.js'); ?>"></script>
 
-            <?= $this->renderSection('scripts'); ?>       
+<?= $this->renderSection('scripts'); ?>
 
-        <!-- Custom JS -->
-        <script src="<?php echo base_url('assets/js/custom.js'); ?>"></script>
-        
-        <!-- Custom-Switcher JS -->
-        <script src="<?php echo base_url('assets/js/custom-switcher.js'); ?>"></script>
+<!-- Custom JS -->
+<script src="<?php echo base_url('assets/js/custom.js'); ?>"></script>
+
+<!-- Custom-Switcher JS -->
+<script src="<?php echo base_url('assets/js/custom-switcher.js'); ?>"></script>
+
+<!-- auto close alert -->
+<!-- auto close alert -->
+<script>
+    setTimeout(function () {
+        var toastContainer = document.querySelector('.toast-container');
+        if (toastContainer) {
+            toastContainer.style.transform = 'translateY(-200%)';
+        }
+    }, 3000);
+</script>
